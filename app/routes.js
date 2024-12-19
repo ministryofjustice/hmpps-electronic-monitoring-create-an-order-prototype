68f5e63b6c8e5bf8743d56c46e52f18a30fbf56a
lisamarierae <75981392+lisamarierae@users.noreply.github.com>
2024-12-19 09:52:21 +0000

    Tidy up on session data

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,35 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
+
+
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Logging session data 
+ 
+ router.use((req, res, next) => { 
+ const log = { 
+ method: req.method, 
+ url: req.originalUrl, 
+ data: req.session.data 
+ } 
+ console.log(JSON.stringify(log, null, 2)) 
+
+ next() 
+ })
+
+ // GET SPRINT NAME - useful for relative templates
+router.use('/', (req, res, next) => {
+  res.locals.currentURL = req.originalUrl; //current screen
+  res.locals.prevURL = req.get('Referrer'); // previous screen
+console.log('previous page is: ' + res.locals.prevURL + " and current page is " + req.url + " " + res.locals.currentURL );
+  next();
+});
+
+
 
 router.get('*', function (req, res, next) {
   // These functions are available on all pages in the prototype.
