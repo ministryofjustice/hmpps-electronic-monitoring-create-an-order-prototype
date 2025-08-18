e10c14af5a884ee7578ff8ff6fe3d3b1df047c7c
lisamarierae <75981392+lisamarierae@users.noreply.github.com>
2025-08-18 09:47:45 +0100

    revert notify

--- app/routes.js
+++ app/routes.js
@@ -737,32 +737,6 @@ router.post('/trail-dates-question', function(request, response) {
     }
 })
 
-// Notify plug in to send emails
-
-var NotifyClient = require('notifications-node-client').NotifyClient,
-    notify = new NotifyClient(process.env.NOTIFYAPIKEY);
-
-// The URL here needs to match the URL of the page that the user is on
-// when they type in their email address
-router.post('/new-form', function (req, res) {
-
-  notify.sendEmail(
-    // this long string is the template ID, copy it from the template
-    // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
-    // in your code.
-    '14f6c101-34c4-4993-af32-968c1b7d1a6a',
-    // `emailAddress` here needs to match the name of the form field in
-    // your HTML page
-    req.body.emailAddress
-  );
-
-  // This is the URL the users will be redirected to once the email
-  // has been sent
-  res.redirect('/v1/submit-confirmation?variation=false');
-
-});
-
-
 
 
 
